2615cdce0da2314ba41256d2ddf4f986eea83554
Haydon J. Korfhage <haydon.korfhage@centre.edu>
2020-02-27 15:33:22 -0500

    Lab 7

--- game.js
+++ game.js
@@ -1,2 +1,20 @@
-<script>
-</script>
+d3.select("body")
+.append("img")
+.attr("src", "wally.jfif")
+.on("click", function()
+   {
+    d3.select(this)
+    .attr("src", "Checkmark.jfif")
+    
+})
+
+d3.select("body")
+.append("img")
+.attr("src", "Cezary.jfif")
+.on("click", function()
+   {
+    d3.select(this)
+    .attr("src", "RedX.png")
+})
+
+
